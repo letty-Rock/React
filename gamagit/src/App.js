56387e7c91d83c721c28fee38fe868c424bdac53
letty-Rock <leticiasantos.roque@outlook.com>
2021-07-13 16:41:51 -0300

    Update App.js

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  return <h1>Hello Gama Academy</h1>
+import React, { useState } from 'react'
+import axios from 'axios'
+function App(props) {
+  const [usuario, setUsuario] = useState('')
+  function handlePesquisa() {
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then(response => console.log(response.data))
+  }
+  return (
+    <>
+      <input
+        className="usuarioInput"
+        placeholder="usuário"
+        value={usuario}
+        onChange={e => setUsuario(e.target.value)}
+      />
+      <button type="button" onClick={handlePesquisa}>
+        Pesquisar
+      </button>
+    </>
+  )
 }
 
 export default App
