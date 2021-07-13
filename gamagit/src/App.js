2a0dabb1fe9f155dc14ada13a5f5d0d8e283c937
letty-Rock <leticiasantos.roque@outlook.com>
2021-07-13 17:16:37 -0300

    Update App.js

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,25 +1,9 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-function App(props) {
-  const [usuario, setUsuario] = useState('')
-  function handlePesquisa() {
-    axios
-      .get(`https://api.github.com/users/${usuario}/repos`)
-      .then(response => console.log(response.data))
-  }
-  return (
-    <>
-      <input
-        className="usuarioInput"
-        placeholder="usuário"
-        value={usuario}
-        onChange={e => setUsuario(e.target.value)}
-      />
-      <button type="button" onClick={handlePesquisa}>
-        Pesquisar
-      </button>
-    </>
-  )
+import React from 'react'
+
+import Routes from './routes'
+
+function App() {
+  return <Routes />
 }
 
 export default App
